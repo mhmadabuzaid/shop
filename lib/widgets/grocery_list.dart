d4ebc0cfaa8c8d19a56f62dc57750be7a247b59c
mhmad abuzaid <mhmadabuzaid99@gmail.com>
2025-12-29 02:32:34 +0300

    finished the new item page and the list

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -1,33 +1,68 @@
 import 'package:flutter/material.dart';
-import 'package:shopping_app/data/dummy_item.dart';
+import 'package:shopping_app/models/grocery_item.dart';
 import 'package:shopping_app/widgets/newitem.dart';
 
-class GroceryList extends StatelessWidget {
+class GroceryList extends StatefulWidget {
   const GroceryList({super.key});
 
   @override
+  State<GroceryList> createState() => _GroceryListState();
+}
+
+class _GroceryListState extends State<GroceryList> {
+  Widget content = Center(child: Text('No Items added ...'));
+  final List<GroceryItem> _groceryItems = [];
+  void _addItem() async {
+    final savedItem = await Navigator.of(
+      context,
+    ).push<GroceryItem>(MaterialPageRoute(builder: (ctx) => Newitem()));
+
+    if (savedItem == null) {
+      return;
+    }
+    setState(() {
+      _groceryItems.add(savedItem);
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Your Grocaries')),
-      body: ListView.builder(
-        itemCount: groceryItems.length,
-        itemBuilder: (ctx, item) {
-          return ListTile(
-            title: Text(groceryItems[item].name),
-            leading: Container(
-              width: 24,
-              height: 24,
-              color: groceryItems[item].category.color,
-            ),
-            trailing: Text(groceryItems[item].quantity.toString()),
-            onTap: () => Navigator.of(context).push(
-              MaterialPageRoute(
-                builder: (ctx) => Newitem(newgroceryItem: groceryItems[item]),
+    // 1. Create a local variable for the content
+    Widget activeContent = const Center(child: Text('No Items added ...'));
+
+    // 2. If items exist, switch activeContent to the ListView
+    if (_groceryItems.isNotEmpty) {
+      activeContent = ListView.builder(
+        itemCount: _groceryItems.length,
+        itemBuilder: (ctx, index) {
+          // rename 'item' to 'index' for clarity
+          return Dismissible(
+            onDismissed: (direction) => setState(() {
+              _groceryItems.removeAt(index);
+            }),
+
+            key: ValueKey(_groceryItems[index].id),
+            child: ListTile(
+              title: Text(_groceryItems[index].name),
+              leading: Container(
+                width: 24,
+                height: 24,
+                color: _groceryItems[index].category.color,
               ),
+              trailing: Text(_groceryItems[index].quantity.toString()),
             ),
           );
         },
+      );
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Groceries'),
+        actions: [IconButton(onPressed: _addItem, icon: const Icon(Icons.add))],
       ),
+      // 3. Use the local variable here
+      body: activeContent,
     );
   }
 }
