14b64fc5c61f0a729bd0f72b5645ec3acf3c729f
mhmad abuzaid <mhmadabuzaid99@gmail.com>
2025-12-31 15:55:59 +0300

    finished the http with firebase

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -1,6 +1,9 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:shopping_app/data/catagories.dart';
 import 'package:shopping_app/models/grocery_item.dart';
 import 'package:shopping_app/widgets/newitem.dart';
+import 'package:http/http.dart' as http;
 
 class GroceryList extends StatefulWidget {
   const GroceryList({super.key});
@@ -10,8 +13,54 @@ class GroceryList extends StatefulWidget {
 }
 
 class _GroceryListState extends State<GroceryList> {
-  Widget content = Center(child: Text('No Items added ...'));
-  final List<GroceryItem> _groceryItems = [];
+  List<GroceryItem> _groceryItems = [];
+  late Future<List<GroceryItem>> _loadedItems;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadedItems = _load();
+  }
+
+  Future<List<GroceryItem>> _load() async {
+    final url = Uri.https(
+      'shoping-list-8df90-default-rtdb.europe-west1.firebasedatabase.app',
+      'shopping-list.json',
+    );
+
+    final response = await http.get(url);
+
+    if (response.statusCode >= 400) {
+      throw Exception('Failed to fetch grocery list. Please try again later.');
+    }
+
+    if (response.body == 'null') {
+      return [];
+    }
+
+    final Map<String, dynamic> listData = json.decode(response.body);
+    final List<GroceryItem> loadedItems = [];
+
+    for (final item in listData.entries) {
+      final category = categories.entries
+          .firstWhere(
+            (catItem) => catItem.value.title == item.value['category'],
+          )
+          .value;
+
+      loadedItems.add(
+        GroceryItem(
+          id: item.key,
+          name: item.value['name'],
+          quantity: item.value['quantity'],
+          category: category,
+        ),
+      );
+    }
+
+    return loadedItems;
+  }
+
   void _addItem() async {
     final savedItem = await Navigator.of(
       context,
@@ -20,49 +69,112 @@ class _GroceryListState extends State<GroceryList> {
     if (savedItem == null) {
       return;
     }
+
     setState(() {
       _groceryItems.add(savedItem);
     });
   }
 
+  void removeItem(GroceryItem item) async {
+    final itemIndex = _groceryItems.indexOf(item);
+
+    setState(() {
+      _groceryItems.remove(item);
+    });
+
+    final url = Uri.https(
+      'shoping-list-8df90-default-rtdb.europe-west1.firebasedatabase.app',
+      'shopping-list/${item.id}.json',
+    );
+
+    try {
+      final response = await http.delete(url);
+
+      if (response.statusCode >= 400) {
+        setState(() {
+          _groceryItems.insert(itemIndex, item);
+        });
+
+        if (context.mounted) {
+          ScaffoldMessenger.of(context).clearSnackBars();
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(content: Text('Could not delete item.')),
+          );
+        }
+      }
+    } catch (error) {
+      setState(() {
+        _groceryItems.insert(itemIndex, item);
+      });
+      if (context.mounted) {
+        ScaffoldMessenger.of(context).clearSnackBars();
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('No internet connection.')),
+        );
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    // 1. Create a local variable for the content
-    Widget activeContent = const Center(child: Text('No Items added ...'));
-
-    // 2. If items exist, switch activeContent to the ListView
-    if (_groceryItems.isNotEmpty) {
-      activeContent = ListView.builder(
-        itemCount: _groceryItems.length,
-        itemBuilder: (ctx, index) {
-          // rename 'item' to 'index' for clarity
-          return Dismissible(
-            onDismissed: (direction) => setState(() {
-              _groceryItems.removeAt(index);
-            }),
-
-            key: ValueKey(_groceryItems[index].id),
-            child: ListTile(
-              title: Text(_groceryItems[index].name),
-              leading: Container(
-                width: 24,
-                height: 24,
-                color: _groceryItems[index].category.color,
-              ),
-              trailing: Text(_groceryItems[index].quantity.toString()),
-            ),
-          );
-        },
-      );
-    }
-
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Groceries'),
         actions: [IconButton(onPressed: _addItem, icon: const Icon(Icons.add))],
       ),
-      // 3. Use the local variable here
-      body: activeContent,
+      body: FutureBuilder(
+        future: _loadedItems,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+
+          if (snapshot.hasError) {
+            return Center(child: Text(snapshot.error.toString()));
+          }
+
+          if (snapshot.hasData) {
+            // FIX: One-time Sync
+            // If our local list is empty, but the server sent data, fill our local list.
+            if (_groceryItems.isEmpty && snapshot.data!.isNotEmpty) {
+              _groceryItems = snapshot.data!;
+            }
+
+            if (_groceryItems.isEmpty) {
+              return const Center(child: Text('No Items added ...'));
+            }
+
+            return ListView.builder(
+              itemCount: _groceryItems.length,
+              itemBuilder: (ctx, index) {
+                return Dismissible(
+                  onDismissed: (direction) {
+                    removeItem(_groceryItems[index]);
+                  },
+                  key: ValueKey(_groceryItems[index].id),
+                  background: Container(
+                    color: Theme.of(
+                      context,
+                    ).colorScheme.error.withOpacity(0.75),
+                    margin: const EdgeInsets.symmetric(horizontal: 16),
+                  ),
+                  child: ListTile(
+                    title: Text(_groceryItems[index].name),
+                    leading: Container(
+                      width: 24,
+                      height: 24,
+                      color: _groceryItems[index].category.color,
+                    ),
+                    trailing: Text(_groceryItems[index].quantity.toString()),
+                  ),
+                );
+              },
+            );
+          }
+
+          return const Center(child: Text('No Items added ...'));
+        },
+      ),
     );
   }
 }
--- lib/widgets/newitem.dart
+++ lib/widgets/newitem.dart
@@ -1,7 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:shopping_app/data/catagories.dart';
 import 'package:shopping_app/models/catagory.dart';
 import 'package:shopping_app/models/grocery_item.dart';
+import 'package:http/http.dart' as http;
 
 class Newitem extends StatefulWidget {
   const Newitem({super.key});
@@ -15,24 +18,64 @@ class _NewitemState extends State<Newitem> {
   var enterdName = "";
   var enteredQuantity = 1;
   var _selectedCategory = categories[Categories.vegetables]!;
+  var isSending = false;
 
-  void _saveItem() {
-    if (_formKey.currentState!.validate()) {
+  void _saveItem() async {
+    print("1. Button clicked!"); // Check if button works
+    // Validate the form
+    final isValid = _formKey.currentState!.validate();
+    print("2. Form is valid: $isValid");
+    if (isValid) {
       _formKey.currentState!.save();
-      // This updates the variables
-      Navigator.of(context).pop(
-        GroceryItem(
-          id: DateTime.now().toString(),
-          name: enterdName,
-          quantity: enteredQuantity,
-          category: _selectedCategory,
-        ),
-      );
+      isSending = true;
 
-      // ONLY proceed if valid
-      print(enterdName);
-      print(enteredQuantity);
-      print(_selectedCategory);
+      print("3. Form saved. Name: $enterdName, Quantity: $enteredQuantity");
+      final url = Uri.https(
+        'shoping-list-8df90-default-rtdb.europe-west1.firebasedatabase.app',
+        'shopping-list.json',
+      );
+      print("4. Sending request to: $url");
+      try {
+        final response = await http.post(
+          url,
+          headers: {'Content-Type': 'application/json'},
+          body: json.encode({
+            "name": enterdName,
+            "quantity": enteredQuantity,
+            "category": _selectedCategory.title,
+          }),
+        );
+
+        print("5. Response received!");
+        print("Status Code: ${response.statusCode}");
+        print("Body: ${response.body}");
+
+        if (response.statusCode >= 200 && response.statusCode < 300) {
+          print("6. SUCCESS! Item saved to database.");
+
+          if (!context.mounted) return;
+
+          final resData = json.decode(response.body);
+          Navigator.of(context).pop(
+            GroceryItem(
+              id: resData['name'],
+              name: enterdName,
+              quantity: enteredQuantity,
+              category: _selectedCategory,
+            ),
+          );
+
+          // Uncomment this to close the screen on success
+          // Navigator.of(context).pop(GroceryItem(...));
+        } else {
+          print("6. FAILED. Server rejected the request.");
+        }
+      } catch (error) {
+        print("CRITICAL ERROR: Could not send request.");
+        print(error); // This prints if you have no internet or bad URL
+      }
+    } else {
+      print("2. Validation FAILED. Check your input fields (red error text).");
     }
   }
 
@@ -112,13 +155,24 @@ class _NewitemState extends State<Newitem> {
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: [
                   TextButton(
-                    onPressed: () {
-                      _formKey.currentState!.reset();
-                    },
+                    onPressed: isSending
+                        ? null
+                        : () {
+                            _formKey.currentState!.reset();
+                          },
                     child: Text('Reset..'),
                   ),
                   SizedBox(width: 8),
-                  ElevatedButton(onPressed: _saveItem, child: Text('Add Item')),
+                  ElevatedButton(
+                    onPressed: isSending ? null : _saveItem,
+                    child: isSending
+                        ? SizedBox(
+                            height: 16,
+                            width: 16,
+                            child: CircularProgressIndicator(),
+                          )
+                        : Text('Add Item'),
+                  ),
                 ],
               ),
             ],
